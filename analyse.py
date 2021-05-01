52d760d37461549b0089273f38b963890b3e24b5
Niklas Saari <niklas.saari@tutanota.com>
2021-05-01 11:45:37 +0300

    Add logger just in case

--- analyse.py
+++ analyse.py
@@ -3,6 +3,9 @@ import csv
 import networkx
 import pathlib
 import datetime
+import logging
+import argparse
+import sys
 from typing import Union, List
 
 
@@ -26,10 +29,17 @@ class TweetData:
         self.username: str = username
         self.description: str = description
         self.location: str = location
-        self.followers: int = followers
-        self.number_statuses: int = number_statuses
-        self.time: datetime.datetime = time
+        self.followers: int = int(followers)
+        self.number_statuses: int = int(number_statuses)
+        if time:
+            self.time: datetime.datetime = self.format_str_time(time)
         self.tweet: str = tweet
+
+    def format_str_time(self, time: str) -> datetime.datetime:
+        """Convert string time to datetime object
+        Example date: month/day/year 1/6/2015 21:07
+        """
+        return datetime.datetime.strptime(time, "%m/%d/%Y %H:%M")
 
 
 def read_csv_into_obj(path: pathlib) -> List[TweetData]:
@@ -42,6 +52,9 @@ def read_csv_into_obj(path: pathlib) -> List[TweetData]:
             if index == 0:
                 continue
             tweets.append(TweetData(*row))
+            if not tweets[index - 1].username and not tweets[index - 1].tweet:
+                GLOBAL_LOGGER.debug(f"Invalid data on row {tweets[index - 1].row_id}")
+    GLOBAL_LOGGER.info("CSV file loaded.")
     return tweets
 
 
@@ -53,9 +66,35 @@ def main():
         raise FileNotFoundError(
             f"CSV dataset not found from the path {DATA_PATH}. Please, download and convert it."
         )
-
     data = read_csv_into_obj(DATA_PATH)
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-l",
+        "--log",
+        dest="log_level",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Set the logging level",
+        default=None,
+    )
+    if len(sys.argv) >= 1 and len(sys.argv) < 3:
+        args = parser.parse_args(args=sys.argv[1:])
+    else:
+        raise ValueError("Too many program arguments provided.")
+
+    log_level = args.log_level if args.log_level else "INFO"
+    if log_level not in {"DEBUG"}:
+        sys.tracebacklimit = 0  # avoid track traces unless debugging
+    logging.basicConfig(
+        format=f"%(levelname)s - %(name)s: %(message)s",
+        level=getattr(logging, log_level),
+    )
+
+    GLOBAL_LOGGER = logging.getLogger("main")
+
     main()
